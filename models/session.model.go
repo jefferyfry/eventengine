0a113cf3f437732fe15607b2e9b17aa53fe1815d
Jeffery Fry <jefferyfry@Jefferys-MacBook-Pro.local>
2024-09-23 12:57:20 -0700

    Updates.

--- models/session.model.go
+++ models/session.model.go
@@ -3,17 +3,17 @@ package models
 import "time"
 
 type Session struct {
-	Name          string    `json:"name" binding:"required"`
-	InstanceType  string    `json:"instanceType" binding:"required"`
-	LwUrl         string    `json:"lwUrl"`
-	LwSubAccount  string    `json:"lwSubAccount"`
-	LwAccessKeyID string    `json:"lwAccessKeyID"`
-	LwSecretKey   string    `json:"lwSecretKey"`
-	LwUserGroup   string    `json:"lwUserGroup"`
-	CreatedBy     string    `json:"createdBy"`
-	UpdatedBy     string    `json:"updatedBy"`
-	CreatedAt     time.Time `json:"createdAt"`
-	UpdatedAt     time.Time `json:"updatedAt"`
-	ExpiresAt     time.Time `json:"expiresAt" binding:"required"`
-	RegCount      int       `json:"regCount"`
+	Name          string    `json:"name" binding:"required" bson:"name"`
+	InstanceType  string    `json:"instanceType" bson:"instanceType" binding:"required"`
+	LwUrl         string    `json:"lwUrl" bson:"lwUrl"`
+	LwSubAccount  string    `json:"lwSubAccount" bson:"lwSubAccount"`
+	LwAccessKeyID string    `json:"lwAccessKeyID" bson:"lwAccessKeyID"`
+	LwSecretKey   string    `json:"lwSecretKey" bson:"lwSecretKey"`
+	LwUserGroup   string    `json:"lwUserGroup" bson:"lwUserGroup"`
+	CreatedBy     string    `json:"createdBy" bson:"createdBy"`
+	UpdatedBy     string    `json:"updatedBy" bson:"updatedBy"`
+	CreatedAt     time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt     time.Time `json:"updatedAt" bson:"updatedAt"`
+	ExpiresAt     time.Time `json:"expiresAt" bson:"expiresAt" binding:"required"`
+	RegCount      int       `json:"regCount" bson:"regCount"`
 }
--- services/session.service.impl.go
+++ services/session.service.impl.go
@@ -3,11 +3,13 @@ package services
 import (
 	"context"
 	"errors"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"fmt"
 	"github.com/jefferyfry/eventengine/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
 	"time"
 )
 
@@ -20,46 +22,38 @@ func NewSessionServiceImpl(ctx context.Context) SessionService {
 }
 
 func (s SessionServiceImpl) GetSessionByName(name string) (*models.Session, error) {
-	db := db.GetDB()
-	params := &dynamodb.GetItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"name": {
-				S: aws.String(name),
-			},
-		},
-		TableName:      aws.String("Sessions"),
-		ConsistentRead: aws.Bool(true),
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+	filter := bson.D{{"name", name}}
+	var session *models.Session
+	err := client.Database("eventengine").Collection("sessions").FindOne(context.Background(), filter).Decode(&session)
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return nil, errors.New(fmt.Sprintf("No session was found with the name %s\n", name))
 	}
-	resp, err := db.GetItem(params)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
-	if len(resp.Item) != 0 {
-		var session *models.Session
-		if err := dynamodbattribute.UnmarshalMap(resp.Item, &session); err != nil {
-			log.Printf("FindAllSessions error with UnmarshalMap : %s", err)
-			return nil, err
-		}
-		return session, nil
-	}
-	return nil, nil
+	return session, nil
 }
 
 func (s SessionServiceImpl) GetAllSessions() ([]models.Session, error) {
-	db := db.GetDB()
-	params := &dynamodb.ScanInput{
-		TableName:      aws.String("Sessions"),
-		ConsistentRead: aws.Bool(true),
-	}
-	resp, err := db.Scan(params)
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+	filter := bson.D{{}}
+	var sessions []models.Session
+	cursor, err := client.Database("eventengine").Collection("sessions").Find(context.Background(), filter)
 	if err != nil {
-		log.Println(err)
 		return nil, err
 	}
-	var sessions []models.Session
-	if err := dynamodbattribute.UnmarshalListOfMaps(resp.Items, &sessions); err != nil {
-		log.Printf("FindAllSessions error with UnmarshalListOfMaps : %s", err)
+	if err = cursor.All(context.TODO(), &sessions); err != nil {
 		return nil, err
 	}
 	return sessions, nil
@@ -73,22 +67,17 @@ func (s SessionServiceImpl) AddSession(session *models.Session) (*models.Session
 	if sessionCheck != nil {
 		return nil, errors.New("Session already exists.")
 	}
-	db := db.GetDB()
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+
 	session.CreatedAt = time.Now()
 	session.UpdatedAt = session.CreatedAt
-	dymap, err := dynamodbattribute.MarshalMap(session)
+	_, err = client.Database("eventengine").Collection("sessions").InsertOne(context.TODO(), session)
 	if err != nil {
-		log.Fatalf("Got error marshalling new session item: %s", err)
-	}
-
-	input := &dynamodb.PutItemInput{
-		Item:      dymap,
-		TableName: aws.String("Sessions"),
-	}
-
-	_, err = db.PutItem(input)
-	if err != nil {
-		log.Printf("Got error calling PutItem: %s", err)
 		return nil, err
 	}
 
@@ -96,50 +85,24 @@ func (s SessionServiceImpl) AddSession(session *models.Session) (*models.Session
 }
 
 func (s SessionServiceImpl) UpdateSession(name string, session *models.Session) (*models.Session, error) {
-	db := db.GetDB()
-	session.UpdatedAt = time.Now()
-
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":url": {
-				S: aws.String(session.LwUrl),
-			},
-			":sub": {
-				S: aws.String(session.LwSubAccount),
-			},
-			":key": {
-				S: aws.String(session.LwAccessKeyID),
-			},
-			":sec": {
-				S: aws.String(session.LwSecretKey),
-			},
-			":usr": {
-				S: aws.String(session.LwUserGroup),
-			},
-			":exp": {
-				S: aws.String(session.ExpiresAt.Format(time.RFC3339)),
-			},
-			":upd": {
-				S: aws.String(session.UpdatedAt.Format(time.RFC3339)),
-			},
-			":upb": {
-				S: aws.String(session.UpdatedBy),
-			},
-		},
-		TableName: aws.String("Sessions"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"name": {
-				S: aws.String(name),
-			},
-		},
-		ReturnValues: aws.String("ALL_NEW"),
-		UpdateExpression: aws.String("set lwUrl = :url,lwSubAccount =:sub, lwAccessKeyID = :key," +
-			"lwSecretKey = :sec, lwUserGroup = :usr, expiresAt = :exp,updatedAt = :upd,updatedBy = :upb"),
-	}
-
-	_, err := db.UpdateItem(input)
+	sessionCheck, err := s.GetSessionByName(session.Name)
 	if err != nil {
-		log.Printf("Got error calling UpdateItem: %s", err)
+		return nil, err
+	}
+	if sessionCheck == nil {
+		return nil, errors.New("Session does not exist.")
+	}
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	session.UpdatedAt = time.Now()
+	filter := bson.D{{"name", name}}
+	_, err = client.Database("eventengine").Collection("sessions").UpdateOne(context.TODO(), filter, session)
+	if err != nil {
 		return nil, err
 	}
 
@@ -147,21 +110,26 @@ func (s SessionServiceImpl) UpdateSession(name string, session *models.Session) 
 }
 
 func (s SessionServiceImpl) DeleteSession(name string) error {
-	db := db.GetDB()
-	input := &dynamodb.DeleteItemInput{
-		Key: map[string]*dynamodb.AttributeValue{
-			"name": {
-				S: aws.String(name),
-			},
-		},
-		TableName: aws.String("Sessions"),
-	}
-
-	_, err := db.DeleteItem(input)
+	sessionCheck, err := s.GetSessionByName(name)
 	if err != nil {
-		log.Printf("Got error calling DeleteItem: %s", err)
 		return err
 	}
+	if sessionCheck == nil {
+		errors.New("Session does not exist.")
+	}
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	filter := bson.D{{"name", name}}
+	_, err = client.Database("eventengine").Collection("sessions").DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -174,28 +142,44 @@ func (s SessionServiceImpl) DeleteSessions(sessions []string) error {
 	return nil
 }
 
-func (s SessionServiceImpl) IncrementSessionRegCount(sessionName string) error {
-	db := db.GetDB()
-
-	input := &dynamodb.UpdateItemInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":inc": {N: aws.String("1")},
-		},
-		TableName: aws.String("Sessions"),
-		Key: map[string]*dynamodb.AttributeValue{
-			"name": {
-				S: aws.String(sessionName),
-			},
-		},
-		ReturnValues:     aws.String("ALL_NEW"),
-		UpdateExpression: aws.String("set regCount = regCount + :inc"),
-	}
-
-	_, err := db.UpdateItem(input)
+func (s SessionServiceImpl) IncrementSessionRegCount(name string) error {
+	session, err := s.GetSessionByName(name)
 	if err != nil {
-		log.Printf("Got error calling UpdateItem: %s", err)
+		return err
+	}
+	if session == nil {
+		return errors.New("Session does not exist.")
+	}
+	client := getMongoClient()
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	session.UpdatedAt = time.Now()
+	session.RegCount += 1
+	filter := bson.D{{"name", name}}
+	_, err = client.Database("eventengine").Collection("sessions").UpdateOne(context.TODO(), filter, session)
+	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func getMongoClient() *mongo.Client {
+	credential := options.Credential{
+		AuthSource:    "admin",
+		AuthMechanism: "SCRAM-SHA-256",
+		Username:      os.Getenv("mongo_usr"),
+		Password:      os.Getenv("mongo_pwd"),
+	}
+	mongoHost := os.Getenv("mongo_host")
+	clientOptions := options.Client().ApplyURI("mongodb://" + mongoHost + ":27017").SetAuth(credential)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatalf("Error creating mongo client: %s", err)
+	}
+	return client
 }
